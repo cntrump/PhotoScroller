9cd757394ec783a6761ff5e95cf08e2dc70ed643
Lvv.me <cntrump@gmail.com>
2022-01-19 14:47:02 +0800

    Remove storyboard

--- Classes/MultiImageScrollViewController.h
+++ Classes/MultiImageScrollViewController.h
@@ -1,16 +1,15 @@
 /*
-     File: AppDelegate.m
- Abstract: The delegate for the Application. This manages which view controller is added to the window at launch.
- 
+     File: MultiImageScrollViewController.h
+ Abstract: Configures and displays the paging scroll view and handles tiling and page configuration.
   Version: 1.3
- 
+
  Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
  Inc. ("Apple") in consideration of your agreement to the following
  terms, and your use, installation, modification or redistribution of
  this Apple software constitutes acceptance of these terms.  If you do
  not agree with these terms, please do not use, install, modify or
  redistribute this Apple software.
- 
+
  In consideration of your agreement to abide by the following terms, and
  subject to these terms, Apple grants you a personal, non-exclusive
  license, under Apple's copyrights in this original Apple software (the
@@ -26,13 +25,13 @@
  implied, are granted by Apple herein, including but not limited to any
  patent rights that may be infringed by your derivative works or by other
  works in which the Apple Software may be incorporated.
- 
+
  The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
  MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
  THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
  FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
  OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
- 
+
  IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
@@ -41,51 +40,17 @@
  AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
  STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
  POSSIBILITY OF SUCH DAMAGE.
- 
+
  Copyright (C) 2012 Apple Inc. All Rights Reserved.
- 
+
  */
 
-#import "AppDelegate.h"
-#import "PhotoViewController.h"
-#import "MultiImageScrollViewController.h"
+#import <UIKit/UIKit.h>
 
-@implementation AppDelegate
+NS_ASSUME_NONNULL_BEGIN
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    _window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
-    _window.rootViewController = [[MultiImageScrollViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll
-                                                                           navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal
-                                                                                         options:@{ UIPageViewControllerOptionInterPageSpacingKey: @20 }];
-    [_window makeKeyAndVisible];
-
-    // kick things off by making the first page
-    PhotoViewController *pageZero = [PhotoViewController photoViewControllerForPageIndex:0];
-    if (pageZero != nil)
-    {
-        // assign the first page to the pageViewController (our rootViewController)
-        MultiImageScrollViewController *pageViewController = (MultiImageScrollViewController *)self.window.rootViewController;
-        pageViewController.dataSource = self;
-        
-        [pageViewController setViewControllers:@[pageZero]
-                                      direction:UIPageViewControllerNavigationDirectionForward
-                                       animated:NO
-                                     completion:NULL];
-    }
-    return YES;
-}
-
-- (UIViewController *)pageViewController:(UIPageViewController *)pvc viewControllerBeforeViewController:(PhotoViewController *)vc
-{
-    NSUInteger index = vc.pageIndex;
-    return [PhotoViewController photoViewControllerForPageIndex:(index - 1)];
-}
-
-- (UIViewController *)pageViewController:(UIPageViewController *)pvc viewControllerAfterViewController:(PhotoViewController *)vc
-{
-    NSUInteger index = vc.pageIndex;
-    return [PhotoViewController photoViewControllerForPageIndex:(index + 1)];
-}
+@interface MultiImageScrollViewController : UIPageViewController
 
 @end
+
+NS_ASSUME_NONNULL_END
